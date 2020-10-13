46c67eaa9bbddf17a378ae465a6e04b5216676aa
Ahnaaf Al Rafee <ahnaafarafee@gmail.com>
2020-10-13 21:28:33 +0600

    Updates

---
+++ precache-manifest.5b265f4f4f8ec098a8a80c24707da8f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cec6018188aa5aa454158b409042d86b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "347aa3725109ac674876",
+    "url": "/monsters-rolodex/static/css/main.9be95f76.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "347aa3725109ac674876",
+    "url": "/monsters-rolodex/static/js/main.6e9df78e.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
